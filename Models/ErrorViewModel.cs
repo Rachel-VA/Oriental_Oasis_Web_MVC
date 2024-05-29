532bd76ca46e956da867ce247be9e029b400a21a
Rachael Savage <Rac.P.Sav@gmail.com>
2024-05-29 11:38:27 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Oriental_Oasis_Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
